7c0a46487f036957d1ee547394dbfd3b06949d77
Iqra1Aslam <156580973+Iqra1Aslam@users.noreply.github.com>
2025-05-09 11:19:27 +0500

    Delete src directory

--- src/utils/util.ts
+++
@@ -1,7 +0,0 @@
-export function convertToFloat(value: number, decimals: number): number {
-  return value / Math.pow(10, decimals);
-}
-
-export function convertFromFloat(value: number, decimals: number): number {
-  return value * Math.pow(10, decimals);
-}
